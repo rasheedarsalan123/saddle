c73919bbd0eb757838ce688b74884d47083eb4f4
arsalan <arsalan@gmail.come>
2024-10-01 11:20:19 +0500

    chang Modal ul

--- src/Component/AddSaddle/DetailPage.jsx
+++ src/Component/AddSaddle/DetailPage.jsx
@@ -52,67 +52,76 @@ const DetailPage = ({
 
   return (
     <div className="flex justify-center items-center top-0 left-0 right-0 fixed z-10 w-[100%] h-[100%]">
-      <div className=" w-[100%] h-[100%] absolute bg-black opacity-10 z-0"></div>
+      <div className=" w-[100%] h-[100%] absolute bg-black opacity-60 z-0"></div>
 
       <div
         ref={modalRef}
-        className="relative  w-[380px] flex flex-col  z-10 bg-white p-8 rounded-lg shadow-lg"
+        className="relative  w-[380px] h-[440px] flex flex-col justify-center  items-center z-10 bg-white 
+         rounded-lg shadow-lg"
       >
         <div
-            className="flex absolute top-0 right-0 p-4 justify-end cursor-pointer"
+            className="flex absolute top-0 right-0 p-4  opacity-35 text-[#2b364b] justify-end cursor-pointer"
             onClick={() => setAddSaddle(false)}
           >
             <MdClear className=" text-[20px]" />
           </div>
-        <div className=" flex mt-[-10px] h-[120px] items-center justify-center space-x-3">
+        <div className=" flex   items-center justify-center">
           <div
-            className="  text-[40px] mb-0 text-blue-950  font-bold
-         justify-center items-center text-center"
+            className="  
+         justify-center items-center text-center "
           >
-            <h1>ADD HORSE</h1>{" "}
+            <h1 className="add-horse text-[48px]  text-[#2b364b] font-[700]">ADD HORSE</h1>
           </div>
           
         </div>
-        <div className=" flex justify-between  ">
+        <div className=" flex  ">
           <form onSubmit={(e) => e.preventDefault()}>
             <div className=" flex flex-col justify-between space-y-4">
             <div className="flex  flex-col relative h-[56px]">         
-              <label htmlFor="Name" className="  absolute top-0
-              leading-normal translate-y-[-50%] left-0 pr-[12px] pl-[12px] z-1  text-[14px] font-medium font-mono  ">
-             Horse Name  </label> 
+              <label    htmlFor="Name" className=" absolute top-[10px]
+             text-[#2b364b] translate-y-[-50%] leading-[14px] font-[Montserrat]  bg-white left-[10px] 
+             pr-[12px] pl-[12px] z-10  text-[12px]  ">
+             Horse Name:*  </label> 
               <div className=" relative">
               <input
                 name="Name"
                 value={inputVal.Name}
                 onChange={handlerInputvalue}
                 type="text"
-                className="w-[100%] mt-2 p-2 text-[14px] h-[43px]  rounded-[60px] leading-[14px] font-mono border-2 border-[#999] "
+              placeholder="Enter Your Horse Name"
+                className="w-[100%] mt-2 p-5 text-[14px] h-[44px]  rounded-[60px] 
+                leading-[14px]  border border-[#2b364b] "
               />
             </div> </div>
 
             <div className="flex  flex-col relative h-[56px] ">
-            <label htmlFor="Birth Date" className=" absolute top-0
-              leading-normal translate-y-[-50%] left-0 pr-[12px] pl-[12px] z-1
-                text-[14px] font-medium font-mono">
+            <label htmlFor="Birth Date" className=" absolute top-[10px]
+               translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
+                text-[12px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  ">
  
-              Birth Date</label>
+              Birth Date:*</label>
               <div className=" relativ">             
                  <input
                 name="BirthDate"
                 value={inputVal.BirthDate}
                 onChange={handlerInputvalue}
+                placeholder="Select horse DOB"
                 type="date"
-                className="w-[100%] mt-2 p-2 text-[14px] h-[43px]  rounded-[60px] leading-[14px] font-mono border-2 border-[#999] "
+                className="w-[100%]   border border-[#2b364b] mt-2 p-4 text-[14px]
+                 h-[43px] text-[#2b364b] rounded-[60px]
+                 leading-[14px] font-[Montserrat]  "
 
               />
              </div>  </div>
-             <div className="flex  flex-col relative h-[56px]"> 
-            <label htmlFor="Horse Category" className=" absolute top-0
-              leading-normal translate-y-[-50%] left-0 pr-[12px] pl-[12px] z-1
-                text-[14px] font-medium font-mono">Horse Category</label> 
+             <div className="flex  flex-col relative h-[56px] "> 
+            <label htmlFor="Horse Category" className=" absolute top-[10px]
+              leading-[14px] translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
+                text-[12px] font-medium font-[Montserrat] bg-white text-[#2b364b] "
+                >Horse Category:*</label> 
             <div>
               <select
-                className="w-[100%] mt-2 p-2 text-[14px] h-[43px]  rounded-[60px] leading-[14px] font-mono border-2 border-[#999]"
+                className="w-[100%] mt-2 p-2 text-[14px] h-[43px] text-[#2b364b] rounded-[60px]
+                 leading-[14px] font-[Montserrat]  border border-[#2b364b]"
                 name="horse"
                 onChange={handlerInputvalue}
               >
@@ -125,22 +134,23 @@ const DetailPage = ({
              </div> </div>
           </form>
           <div
-            className="bg-[#d9d9d9] items-center text-center w-[100px] justify-center
-             pt-10 pl-10 rounded-xl mt-2
-          ml-5 h-[110px] text-[20px]"
+            className="bg-[#d9d9d9] items-center text-center w-[90px] justify-center
+             pt-9 pl-9 rounded-xl mt-1
+          ml-2 h-[95px] text-[18px]  font-[Montserrat] text-[#2b364b] font-[400] "
           >
             <MdOutlineFileUpload />
           </div>
         </div>
-        <div className=" flex justify-around space-x-5 mt-4">
+        <div className=" flex justify-center gap-6 mt-4">
           <button
             onClick={handlerSave}
-            className="bg-blue-950  text-white p-1 rounded-[30px] w-[130px] "
+            className="bg-[#2b364b]  font-[Montserrat] text-white p-[2px] rounded-[30px] w-[145px] "
           >
             Save
           </button>
           <button
-            className=" border-solid border-2 border-blue-950 p-1 rounded-[30px] w-[140px]"
+            className=" border-solid border-2 border-[#2b364b] text-[#2b364b] 
+            font-[Montserrat] font-[400] p-[6px] h-[42px] rounded-[30px] w-[145px]"
             onClick={() => setAddSaddle(false)}
           >
             Cancel
