7a7eb86a91c7d6c26d2e2d2a6a7245305b13d939
arsalan <arsalan@gmail.come>
2024-10-31 19:29:40 +0500

    added routing

---
+++ src/Component/AddSaddle/BayAsaddle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BayAsaddle = () => {
+  return (
+    <div>
+      <h1>Bay A Saddle</h1>
+    </div>
+  )
+}
+
+export default BayAsaddle
---
+++ src/Component/AddSaddle/MySaddle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MySaddle = () => {
+  return (
+    <div>
+      <h1>My Saddle</h1>
+    </div>
+  )
+}
+
+export default MySaddle
