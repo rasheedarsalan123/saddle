c93bb4f07c6662479f7a0491d39826268b2a58b7
arsalan <arsalan@gmail.come>
2024-10-31 19:52:29 +0500

    changes ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import AddSaddle from './Component/AddSaddle/AddSaddle.jsx'
-import Home from './Component/AddSaddle/Home.jsx'
-import MySaddle from './Component/AddSaddle/MySaddle.jsx'
-import BayAsaddle from './Component/AddSaddle/BayAsaddle.jsx'
-import Schedule from './Component/AddSaddle/Schedule.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddSaddle from "./Component/AddSaddle/AddSaddle.jsx";
+import Home from "./Component/AddSaddle/Home.jsx";
+import MySaddle from "./Component/AddSaddle/MySaddle.jsx";
+import BayAsaddle from "./Component/AddSaddle/BayAsaddle.jsx";
+import Schedule from "./Component/AddSaddle/Schedule.jsx";
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:'/horses',
-        element:<AddSaddle/>},
-        {
-          path:'/home',
-          element:<Home/>},
-          {
-            path:'/mysaddle',
-            element:<MySaddle/>},
-            {
-              path:'/bayasaddle',
-              element:<BayAsaddle/>},
-              {
-                path:'/schedule',
-                element:<Schedule/>}
-
-    ]
-
-    
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+        path: "/horses",
+        element: <AddSaddle />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/mysaddle",
+        element: <MySaddle />,
+      },
+      {
+        path: "/buysaddle",
+        element: <BayAsaddle />,
+      },
+      {
+        path: "/schedule",
+        element: <Schedule />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  
-    <RouterProvider router={router}  />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
