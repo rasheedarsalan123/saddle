5c7beac9e7b544d41fd81454800fea907fc1ef05
arsalan <arsalan@gmail.come>
2024-11-22 21:07:51 +0500

    same ui cahnges

--- src/Component/Partner/Partner.jsx
+++ src/Component/Partner/Partner.jsx
@@ -8,18 +8,21 @@ const Partner = () => {
         <div className=" relative  h-auto w-full ">
           <img src="Component 9.png" alt="" />
         </div>
-                
-             <div className=" absolute">
-                 <div className=" flex justify-center items-center
-               pb-10  ">
-          <h2 className="text-[44px] sm:text-[54px] text-white add-horse font-[700]  md:text-[64px] leading-[100%]">
-            BECOME A PARTNER
-          </h2>{" "}
-        </div> </div>
+
+        <div className=" absolute">
+          <div
+            className=" flex justify-center items-center
+               pb-10  "
+          >
+            <h2 className="text-[44px] sm:text-[54px] text-white add-horse font-[700]  md:text-[64px] leading-[100%]">
+              BECOME A PARTNER
+            </h2>{" "}
+          </div>{" "}
+        </div>
       </div>
-      <div className=" section-2 h-full bg-[#2b364b]  w-auto   pt-24 md-pt-12 pb-14 ">
+      <div className=" section-2 h-full bg-[#2b364b]  w-auto   p-6 pb-14 ">
         <div className=" w-[80%] mx-auto">
-          <h3 className="text-[40px] text-center sm:text-[54px] text-white add-horse font-[700]  md:text-[64px] leading-[100%]">
+          <h3 className=" text-[28px] text-center sm:text-[24px] text-white add-horse font-[700]  md:text-[64px] leading-[100%]">
             WHY BECOME A PARTNER
           </h3>
         </div>
@@ -111,158 +114,175 @@ const Partner = () => {
         </div>
       </div>
       <div className="h-full   w-auto   pt-10 md-pt-6 pb-14">
-        <div className=" w-[80%] mx-[auto]">
+        <div className=" w-[80%] mx-[auto] pb-14">
           <h3 className="text-4xl text-center md:text-5xl text-[#2b364b] add-horse font-[700]   leading-tight mb-12">
             GIVE YOUR BRAND THA SADDLEFIT ADVANTAGE
           </h3>
         </div>
         <div className=" flex justify-around   md:px-[50px] px-[20px] flex-wrap  w-auto h-full   ">
-          <div className=" flex  text-center items-center  
-           rounded-[32px] sm:w-[582px] w-[339px] h-auto shadow-xl ">
-            <div><img src="cowboy-horse.svg" alt="" /></div>
+          <div
+            className=" flex  text-center items-center  
+           rounded-[32px] sm:w-[582px] w-[339px] h-[392px]  "
+          >
+            <div>
+              <img src="cowboy-horse.svg" alt="" />
+            </div>
           </div>
-          <div className=" flex flex-wrap text-center items-center   
-           rounded-[32px] sm:w-[582px] w-[339px] h-auto shadow-xl   px-4 sm:my-0 my-10">
-        
-              <h3 className="text-[16px] text-center md:text-[24px] font-[Montserrat] text-[#2b364b] 
-               font-bold  leading-auto md:leading-[110%] pt-4">
+          <div
+            className=" flex flex-wrap text-center items-center   
+           rounded-[32px] sm:w-[582px] w-[339px] md:h-[392px] h-auto shadow-xl   px-4 "
+          >
+            <h3
+              className="text-[16px] text-center md:text-[24px] font-[Montserrat] text-[#2b364b] 
+               font-bold  leading-auto md:leading-[110%] pt-4"
+            >
               Request Information to Become a Partner.
-              </h3> 
-            
+            </h3>
+
             <div className=" w-full flex flex-col  flex-wrap gap-3">
-                
-            <div className=" relative w-[298px]  sm:w-full flex flex-wrap ">
-                    <label
-                      htmlFor="Birth Date"
-                      className=" absolute top-[10px]
+              <div className=" relative w-[298px]  sm:w-full flex flex-wrap ">
+                <label
+                  htmlFor="Birth Date"
+                  className=" absolute top-[10px]
                  translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
                   text-[14px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  text-opacity-42  "
-                    >
-                      Company 
-                    </label>
-                    <input
-                      name="Name"
-                    //   value={changeText && inputVal.Name}
-                    //   onChange={handlerInputvalue}
-                      type="text"
-                      placeholder="Barrel Racers"
-                      className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
+                >
+                  Company
+                </label>
+                <input
+                  name="Name"
+                  //   value={changeText && inputVal.Name}
+                  //   onChange={handlerInputvalue}
+                  type="text"
+                  placeholder="Barrel Racers"
+                  className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
                 leading-[14px]  border border-[#2b364b] "
-                    />
-                  </div>
-                  <div className="sm:flex  justify-between flex-wrap " >
-                  <div className=" relative w-[298px]  sm:w-[260px] py-2  ">
-                    <label
-                      htmlFor="Birth Date"
-                      className=" absolute top-[10px]
+                />
+              </div>
+              <div className="sm:flex  justify-between flex-wrap ">
+                <div className=" relative w-[298px]  sm:w-[260px] py-2  ">
+                  <label
+                    htmlFor="Birth Date"
+                    className=" absolute top-[10px]
                  translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
                   text-[14px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  text-opacity-42  "
-                    >
-                      Company 
-                    </label>
-                    <input
-                      name="Name"
+                  >
+                    Company
+                  </label>
+                  <input
+                    name="Name"
                     //   value={changeText && inputVal.Name}
                     //   onChange={handlerInputvalue}
-                      type="text"
-                      placeholder="Barrel Racers"
-                      className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
+                    type="text"
+                    placeholder="Barrel Racers"
+                    className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
                 leading-[14px]  border border-[#2b364b] "
-                    />
-                  </div>
-                  <div className=" relative w-[298px]  sm:w-[260px]  py-2  ">
-                    <label
-                      htmlFor="Birth Date"
-                      className=" absolute top-[10px]
+                  />
+                </div>
+                <div className=" relative w-[298px]  sm:w-[260px]  py-2  ">
+                  <label
+                    htmlFor="Birth Date"
+                    className=" absolute top-[10px]
                  translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
                   text-[14px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  text-opacity-42  "
-                    >
-                      Company 
-                    </label>
-                    <input
-                      name="Name"
+                  >
+                    Company
+                  </label>
+                  <input
+                    name="Name"
                     //   value={changeText && inputVal.Name}
                     //   onChange={handlerInputvalue}
-                      type="text"
-                      placeholder="Barrel Racers"
-                      className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
+                    type="text"
+                    placeholder="Barrel Racers"
+                    className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
                 leading-[14px]  border border-[#2b364b] "
-                    />
-                  </div>
-                  </div>
-                  <div className=" sm:flex  justify-between flex-wrap ">
-                  <div className=" relative w-[298px]  sm:w-[260px]  py-2  ">
-                    <label
-                      htmlFor="Birth Date"
-                      className=" absolute top-[10px]
+                  />
+                </div>
+              </div>
+              <div className=" sm:flex  justify-between flex-wrap ">
+                <div className=" relative w-[298px]  sm:w-[260px]  py-2  ">
+                  <label
+                    htmlFor="Birth Date"
+                    className=" absolute top-[10px]
                  translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
                   text-[14px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  text-opacity-42  "
-                    >
-                      Company 
-                    </label>
-                    <input
-                      name="Name"
+                  >
+                    Company
+                  </label>
+                  <input
+                    name="Name"
                     //   value={changeText && inputVal.Name}
                     //   onChange={handlerInputvalue}
-                      type="text"
-                      placeholder="Barrel Racers"
-                      className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
+                    type="text"
+                    placeholder="Barrel Racers"
+                    className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
                 leading-[14px]  border border-[#2b364b] "
-                    />
-                  </div>
-                  <div className=" relative w-[298px]  sm:w-[260px]  py-2 ">
-                    <label
-                      htmlFor="Birth Date"
-                      className=" absolute top-[10px]
+                  />
+                </div>
+                <div className=" relative w-[298px]  sm:w-[260px]  py-2 ">
+                  <label
+                    htmlFor="Birth Date"
+                    className=" absolute top-[10px]
                  translate-y-[-50%] left-[10px] pr-[12px] pl-[12px] 
                   text-[14px] font-[Montserrat] bg-white text-[#2b364b] leading-[14px]  text-opacity-42  "
-                    >
-                      Company 
-                    </label>
-                    <input
-                      name="Name"
+                  >
+                    Company
+                  </label>
+                  <input
+                    name="Name"
                     //   value={changeText && inputVal.Name}
                     //   onChange={handlerInputvalue}
-                      type="text"
-                      placeholder="Barrel Racers"
-                      className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
+                    type="text"
+                    placeholder="Barrel Racers"
+                    className="w-[100%] mt-2 p-5 text-[14px] h-[54px] border-opacity-40  rounded-[60px] 
                 leading-[14px]  border border-[#2b364b] "
-                    />
-                  </div>
-                  </div>
-                  </div>
-                  <button className=" bg-[#2b364b] text-white m-5 font-[Montserrat] py-[7px] px-[35px] rounded-[24px] text-[16px]">Submit</button>
+                  />
+                </div>
+              </div>
+            </div>
+            <button className=" bg-[#2b364b] text-white m-5 font-[Montserrat] w-[125px] h-[45px] rounded-[24px] text-[16px]">
+              Submit
+            </button>
           </div>
         </div>
       </div>
 
-      <div className="max-w-[1824px] h-[589px] flex justify-center items-center mb-[10px] relative ">
-  <div className="w-[90%] h-full  ">
-    <img
-      src="heroBanner-bg.png"
-      alt=""
-      className="w-full h-full object-cover rounded-[24px] "
-    />
-  </div>
-  <div className="   max-w-[814px] h-[202px]   absolute  justify-center items-center gap-5
-   flex flex-col  py-3  ">
-   <div className="  w-full  h-full  flex flex-col  p-10  py-3 ">
-     <h4 className=" text-4xl text-center md:text-5xl text-white add-horse font-[700]  py-3  
-      leading-tight ">
-      ETIAM ERAT VELIT</h4>
-    <p className=" W-[814px] h-[51px] text-white  py-3  font-[Montserrat]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores laborum, autem blanditiis temporibus accusantium necessitatibus incidunt quod repellat optio similique perferendis, suscipit dicta cumque ipsum sunt eveniet dolor. Nostrum!
-   
-    </p>
-    </div>
-  <div className=" flex  absolute top-[330px] sm:top-[200px] "><button className=" text-[#2b364b]   bg-white  font-[Montserrat] py-[7px] px-[35px] 
-  rounded-[24px] text-[16px]">Submit</button> </div>
-   
-  </div>
-</div>
-
-
-
-
+      <div className="max-w-[1824px] h-[589px] flex justify-center items-center mb-[10px] relative px-[30px] ">
+        <div className="w-[100%] h-full   ">
+          <img
+            src="heroBanner-bg.png"
+            alt=""
+            className="w-full h-full object-cover rounded-[24px] "
+          />
+        </div>
+        <div
+          className="   max-w-[814px] h-[202px]   absolute  justify-center items-center gap-5
+   flex flex-col  py-3  "
+        >
+          <div className="  w-full  h-full  flex flex-col    py-3 ">
+            <h4
+              className=" text-4xl text-center md:text-5xl text-white add-horse font-[700]  py-3  
+      leading-tight "
+            >
+              ETIAM ERAT VELIT
+            </h4>
+            <p className=" w-[auto]  px-[55px] h-[51px] text-white  text-center py-3 font-[Montserrat]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              maiores laborum, autem blanditiis temporibus accusantium
+              necessitatibus incidunt quod repellat optio similique perferendis,
+              suscipit dicta cumque ipsum sunt eveniet dolor. Nostrum!
+            </p>
+          </div>
+          <div className=" flex  absolute top-[330px] sm:top-[200px] ">
+            <button
+              className=" text-[#2b364b]   bg-white  font-[Montserrat] w-[125px] h-[45px] 
+  rounded-[24px] text-[16px]"
+            >
+              Submit
+            </button>{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
