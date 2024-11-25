7f9a0fe6c7f1ac0de85fee18733fd75065f87d86
arsalan <arsalan@gmail.come>
2024-11-25 17:59:46 +0500

    heading line heigh changes

--- src/Component/Partner/Partner.jsx
+++ src/Component/Partner/Partner.jsx
@@ -150,7 +150,7 @@ const Partner = () => {
           sm:p-[32px] p-[24px] mt-[46px] rounded-[32px] sm:w-[582px] w-full
            max-h-[586px] md:h-[408px] h-auto shadow-xl">
             <h3 className="text-[16px px-2 sm:text-[24px] font-[Montserrat]
-             text-[#2b364b] font-bold mb-[16px]">
+             text-[#2b364b] font-bold mb-[16px] leading-[110%]">
               Request Information to Become a Partner.
             </h3>
 
